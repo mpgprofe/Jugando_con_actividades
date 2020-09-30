b4283df9bb554f337dcce24b813c4d4cd192fb3a
mpgprofe <mpgprofe@gmail.com>
2020-09-30 09:14:28 +0200

    Creo otra actividad

---
+++ app/src/main/java/com/example/jugandoconactividades/SaludoActivity.java
@@ -0,0 +1,14 @@
+package com.example.jugandoconactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaludoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saludo);
+    }
+}
