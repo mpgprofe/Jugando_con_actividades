89f512f55c478340ac3c15c8b132fcc45b24f0d0
mpgprofe <mpgprofe@gmail.com>
2020-09-30 09:38:34 +0200

    Ya funciona

--- app/src/main/java/com/example/jugandoconactividades/MainActivity.java
+++ app/src/main/java/com/example/jugandoconactividades/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.jugandoconactividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button lanzar;
+    EditText nombre;
+
+    public static final String NOMBRE = "Nombre";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lanzar = findViewById(R.id.button);
+        nombre = findViewById(R.id.editTextTextPersonName);
+
+        lanzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intento = new Intent(MainActivity.this, SaludoActivity.class);
+                intento.putExtra(NOMBRE, nombre.getText().toString());
+
+                startActivity(intento); //Lanza la nueva actividad.
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jugandoconactividades/SaludoActivity.java
+++ app/src/main/java/com/example/jugandoconactividades/SaludoActivity.java
@@ -2,13 +2,38 @@ package com.example.jugandoconactividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SaludoActivity extends AppCompatActivity {
+    Button volver;
+    TextView nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+
+        Intent intentoDeFuera = getIntent();
+
+        nombre = findViewById(R.id.textViewNombre);
+
+        String nombrDeFuera = intentoDeFuera.getStringExtra(MainActivity.NOMBRE);
+
+        nombre.setText("Hola " +nombrDeFuera);
+
+
+
+        volver = findViewById(R.id.buttonVolver);
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
